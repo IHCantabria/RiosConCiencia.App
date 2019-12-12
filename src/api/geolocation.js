2b9608482b5ad6116133af6024b3fc45e662b469
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-12 14:45:33 +0100

    Change response to a promise

--- src/api/geolocation.js
+++ src/api/geolocation.js
@@ -1,17 +1,9 @@
 /** Using Geolocation API (HTML5) */
 
-const geolocation = () => {
-  navigator.geolocation.getCurrentPosition(_returnPosition, _errorHandler);
+const getUserGeolocation = () => {
+  return new Promise((res, rej) => {
+    navigator.geolocation.getCurrentPosition(res, rej);
+  });
 };
 
-const _returnPosition = position => {
-  const lat = position.coords.lat;
-  const lon = position.coords.lon;
-  return { lon: lon, lat: lat };
-};
-
-const _errorHandler = error => {
-  console.error(`Error cargando posición. ${error}`);
-};
-
-export { geolocation };
+export { getUserGeolocation };
