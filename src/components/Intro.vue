2768c228df5b5e62ddb33beab2eb42e0872f0a5d
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-18 13:36:24 +0100

    Layout logos

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="intro">
-    <div class="intro__main">
-      <button>Usuario Experto</button>
-      <button>Usuario con Desafíos</button>
+    <div class="intro__title-logo">
+      <img src="@/assets/logos/riosconciencia.png" title="Ríos Con Ciencia" />
     </div>
-    <div class="intro_logos">
-      Logos
+    <div class="intro__main">
+      <section class="hero">
+        <div class="hero-body">
+          <div class="container">
+            <h2 class="subtitle">Usuario Experto</h2>
+            <b-button size="is-large" type="is-primary">Empezar</b-button>
+
+            <h2 class="subtitle">Usuario con Desafíos</h2>
+            <b-button size="is-large" type="is-primary">Empezar</b-button>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="intro__footer-logos">
+      <img
+        src="@/assets/logos/LogoMinisterio-FECYT.jpg"
+        title="Gobierno de España y Fundación Española para la Ciencia y la Tecnología"
+      />
+      <img src="@/assets/logos/RedCambera.jpg" title="Red Cambera" />
+      <img src="@/assets/logos/ProyectoRios.jpg" title="Proyecto Ríos" />
+      <img src="@/assets/logos/ProyectoRios.jpg" title="Proyecto Ríos" />
     </div>
   </div>
 </template>
@@ -19,12 +37,24 @@ export default {
   display: flex;
   flex-grow: 1;
   flex-direction: column;
+  align-items: center;
   &__main {
     display: flex;
     flex-grow: 1;
     height: 50%;
   }
-  &__logos {
+  &__title-logo {
+    padding: 1rem;
+    & img {
+      max-height: 10vh;
+    }
+  }
+  &__footer-logos {
+    padding: 1rem;
+    & img {
+      max-height: 10vh;
+      padding: 1rem;
+    }
   }
 }
 </style>
