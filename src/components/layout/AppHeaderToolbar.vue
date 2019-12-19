975ea8575fa8601987a62603e7873c6b91c5ccf1
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-19 13:43:32 +0100

    Icons for header navigation buttons

--- src/components/layout/AppHeaderToolbar.vue
+++ src/components/layout/AppHeaderToolbar.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="toolbar">
-    <div id="nav">
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/about">Ayuda</router-link>
+    <div class="nav">
+      <div class="nav__link">
+        <router-link to="/"
+          ><b-icon icon="home-circle" size="is-medium"> </b-icon
+        ></router-link>
+      </div>
+      <div class="nav__link">
+        <router-link to="/about"
+          ><b-icon icon="help-circle" size="is-medium"> </b-icon
+        ></router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -15,4 +23,13 @@ export default {
   methods: {}
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  &__link {
+    padding-right: 1rem;
+  }
+}
+</style>
