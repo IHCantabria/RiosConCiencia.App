2d2cafe5ced909cd56952bf1a5fb08161c8b94fe
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-19 12:34:37 +0100

    Empty pages just for navigation

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>Help page</h1>
+  <div clas="container">
+    <div class="section">
+      <h2 class="title box">Acerca de</h2>
+    </div>
+    <h3 class="subtitle is-3">Work in progress...</h3>
   </div>
 </template>
---
+++ src/views/FormPictosView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div clas="container">
+    <div class="section">
+      <h2 class="title box">Ficha de Pictos</h2>
+    </div>
+    <h3 class="subtitle is-3">Work in progress...</h3>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
