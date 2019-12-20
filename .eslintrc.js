984a7dd4f0bb80dc73a93f03fd420b42e8fe8d77
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-20 12:13:55 +0100

    eslint rule no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,12 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off"
   },
+  //   rules: {
+  //     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+  //     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  //   },
   parserOptions: {
     parser: "babel-eslint"
   },
