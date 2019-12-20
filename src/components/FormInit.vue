9bd44d3c97eda4799967332f72b559002c507725
Luis Pedraz <LPedraz1981@gmail.com>
2019-12-20 11:32:13 +0100

    Combo layout for stream

--- src/components/FormInit.vue
+++ src/components/FormInit.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="section">
-    <section>
-      <b-dropdown aria-role="list">
-        <button class="button is-primary" slot="trigger">
-          <span>Click me!</span>
-          <b-icon icon="menu-down"></b-icon>
-        </button>
-
-        <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-        <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-        <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-      </b-dropdown>
-    </section>
-  </div>
+  <section class="form-section">
+    <b-field label="0. Datos Iniciales" class="form-section__title">
+      <b-select placeholder="Seleccione Tramo" rounded>
+        <option value="tr2">Tramo 1</option>
+        <option value="tr1">Tramo 2</option>
+      </b-select>
+    </b-field>
+  </section>
 </template>
 <script>
 export default {};
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form-section {
+  text-align: left;
+  padding: 1rem;
+}
+</style>
