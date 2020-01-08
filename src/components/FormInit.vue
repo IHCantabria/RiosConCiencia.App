3f0791bc356da9052850b884828334e4d8f95291
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-08 14:18:26 +0100

    Form 0 controls design

--- src/components/FormInit.vue
+++ src/components/FormInit.vue
@@ -1,19 +1,49 @@
 <template>
-  <section class="form-section">
-    <b-field label="0. Datos Iniciales" class="form-section__title">
-      <b-select placeholder="Seleccione Tramo" rounded>
+  <div class="form-section">
+    <h5 class="title is-5">0. Datos Iniciales</h5>
+    <b-field label="Nombre del responsable">
+      <label>Luis Pedraz</label>
+    </b-field>
+    <b-field label="Acompañantes">
+      <b-input value=""></b-input>
+    </b-field>
+    <b-field label="Tramo">
+      <b-select placeholder="Seleccione Tramo">
         <option value="tr2">Tramo 1</option>
         <option value="tr1">Tramo 2</option>
       </b-select>
     </b-field>
-  </section>
+    <b-field label="Cuenca / Municipio">
+      <label>Saja / Cabezón de la Sal</label>
+    </b-field>
+    <b-field label="Clima hoy">
+      <b-select placeholder="Seleccione una opción">
+        <option value="sol">Soleado</option>
+        <option value="nubes">Nublado</option>
+        <option value="lluvia">Lluvioso</option>
+      </b-select>
+    </b-field>
+    <b-field label="Clima últimos 2 días">
+      <b-select placeholder="Seleccione una opción">
+        <option value="sol">Soleado</option>
+        <option value="nubes">Nublado</option>
+        <option value="lluvia">Lluvioso</option>
+      </b-select>
+    </b-field>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style lang="scss" scoped>
 .form-section {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
   text-align: left;
-  padding: 1rem;
+  &__title {
+    text-decoration: underline;
+    font-size: 1.2rem;
+  }
 }
 </style>
