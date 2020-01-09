952e39565213a47e9ab41b75682cd2e49e939b8e
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-09 09:56:00 +0100

    Set form step navigation

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="section">
-    <form-init></form-init>
+    <form-init v-show="activeSectionId === formSections.init.id"></form-init>
+    <form-basic v-show="activeSectionId === formSections.basic.id"></form-basic>
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
   components: {
-    "form-init": () => import("@/components/FormInit")
+    "form-init": () => import("@/components/FormInit"),
+    "form-basic": () => import("@/components/FormBasic")
+  },
+  computed: {
+    ...mapState({
+      activeSectionId: state => state.activeSectionId,
+      formSections: state => state.formSections
+    })
   }
 };
 </script>
