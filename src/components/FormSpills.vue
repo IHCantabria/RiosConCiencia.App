336cf197f7e2d2ae9a357663da0133ce96910dc9
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-10 14:53:59 +0100

    Init form

---
+++ src/components/FormSpills.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form-section">
+    <h5 class="title is-5">2. Inspección de Vertidos (500m)</h5>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped></style>
