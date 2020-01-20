1d0dbafd7eb9aa5da706cc00470a88a6dacfd90d
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-20 13:05:40 +0100

    Update init form with dynamic data

--- src/components/FormInit.vue
+++ src/components/FormInit.vue
@@ -2,32 +2,46 @@
   <div class="form-section">
     <h5 class="title is-5">0. Datos Iniciales</h5>
     <b-field label="Nombre del responsable">
-      <label>Luis Pedraz</label>
+      <label>{{ user.name }}</label>
     </b-field>
     <b-field label="Acompañantes">
       <b-input value=""></b-input>
     </b-field>
     <b-field label="Tramo">
-      <b-select placeholder="Seleccione Tramo">
-        <option value="tr2">Tramo 1</option>
-        <option value="tr1">Tramo 2</option>
+      <b-select
+        icon="go-kart-track"
+        placeholder="Seleccione Tramo"
+        v-model="riverSection"
+      >
+        <option
+          v-for="option in riverSections"
+          :key="option.id"
+          :value="option"
+          >{{ option.name }}</option
+        >
       </b-select>
     </b-field>
     <b-field label="Cuenca / Municipio">
-      <label>Saja / Cabezón de la Sal</label>
+      <label>{{ riverSection.cuenca }} / {{ riverSection.municipio }}</label>
     </b-field>
     <b-field label="Clima hoy">
-      <b-select placeholder="Seleccione una opción">
-        <option value="sol">Soleado</option>
-        <option value="nubes">Nublado</option>
-        <option value="lluvia">Lluvioso</option>
+      <b-select icon="weather-lightning-rainy" v-model="weatherToday">
+        <option
+          v-for="option in weatherOptions"
+          :key="option"
+          :value="option"
+          >{{ option }}</option
+        >
       </b-select>
     </b-field>
     <b-field label="Clima últimos 2 días">
-      <b-select placeholder="Seleccione una opción">
-        <option value="sol">Soleado</option>
-        <option value="nubes">Nublado</option>
-        <option value="lluvia">Lluvioso</option>
+      <b-select icon="weather-lightning-rainy" v-model="weather48h">
+        <option
+          v-for="option in weatherOptions"
+          :key="option"
+          :value="option"
+          >{{ option }}</option
+        >
       </b-select>
     </b-field>
   </div>
@@ -38,8 +52,34 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState({
-      activeSectionId: state => state.activeSectionId
+      activeSectionId: state => state.activeSectionId,
+      user: state => state.user
     })
+  },
+  mounted() {
+    this.init();
+  },
+  data() {
+    return {
+      riverSection: {},
+      weatherToday: "",
+      weather48h: "",
+      weatherOptions: ["Soleado", "Nublado", "Lluvioso", "Tormenta"],
+      riverSections: [
+        {
+          id: 1,
+          name: "tramo 1",
+          cuenca: "Saja",
+          municipio: "Cabezón de la Sal"
+        },
+        { id: 2, name: "tramo 2", cuenca: "Saja", municipio: "Cabuerniga" }
+      ]
+    };
+  },
+  methods: {
+    init() {
+      this.riverSection = this.riverSections[0];
+    }
   }
 };
 </script>
