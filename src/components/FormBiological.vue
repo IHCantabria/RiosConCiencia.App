19fb8b3d9533a2b5f3004f051d6ff8734fe5844c
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-21 15:07:58 +0100

    Biological form template

---
+++ src/components/FormBiological.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="form-section">
+    <h5 class="title is-5">6. Calidad Biológica del agua</h5>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
