cfc721f00f7eebc6fbf83934c5481d5ecc79a0c3
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-23 13:55:45 +0100

    component template

---
+++ src/components/FormEcoResult.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="form-section">
+    <h5 class="title is-5">8. Estado ecológico</h5>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
