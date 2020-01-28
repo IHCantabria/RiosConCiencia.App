08d0adb98007be4c41e02cd837e0dbfc1bfcb35a
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-28 09:08:15 +0100

    Bind form data to store

--- src/components/FormEcosystem.vue
+++ src/components/FormEcosystem.vue
@@ -13,7 +13,7 @@
           max="180"
           step="any"
           custom-class="is-small"
-          v-model="samplePointCoords.lon"
+          v-model="values.samplePointCoords.lon"
         >
         </b-input>
       </b-field>
@@ -25,7 +25,7 @@
           max="90"
           step="any"
           custom-class="is-small"
-          v-model="samplePointCoords.lat"
+          v-model="values.samplePointCoords.lat"
         >
         </b-input>
       </b-field>
@@ -42,14 +42,20 @@
       >
     </b-field>
     <b-field label="Anchura (m)" custom-class="is-small">
-      <b-numberinput v-model="samplePointWidth" step="0.1"></b-numberinput>
+      <b-numberinput
+        v-model="values.samplePointWidth"
+        step="0.1"
+      ></b-numberinput>
     </b-field>
     <b-field label="Profundidad media (m)" custom-class="is-small">
-      <b-numberinput v-model="samplePointDepth" step="0.1"></b-numberinput>
+      <b-numberinput
+        v-model="values.samplePointDepth"
+        step="0.1"
+      ></b-numberinput>
     </b-field>
     <b-field label="Velocidad del agua (m/s)" custom-class="is-small">
       <b-numberinput
-        v-model="samplePointWaterVelocity"
+        v-model="values.samplePointWaterVelocity"
         step="0.1"
       ></b-numberinput>
     </b-field>
@@ -58,7 +64,7 @@
     <div class="is-divider"></div>
     <b-field label="b. Características físicas del agua"> </b-field>
     <b-field label="Temperatura (°C)" custom-class="is-small">
-      <b-numberinput v-model="samplePointWaterTemp"></b-numberinput>
+      <b-numberinput v-model="values.samplePointWaterTemp"></b-numberinput>
     </b-field>
     <b-field label="Transparencia" custom-class="is-small"> </b-field>
     <b-field
@@ -68,9 +74,9 @@
     ></b-field>
     <div class="field">
       <b-checkbox
-        v-for="(option, index) in formEcoSystem.data.transparencyOptions"
-        :key="index"
-        v-model="samplePointWaterTransparency"
+        v-for="option in formEcoSystem.data.transparencyOptions"
+        :key="option.id"
+        v-model="values.samplePointWaterTransparency"
         :native-value="option"
         >{{ option.name }}
       </b-checkbox>
@@ -80,7 +86,7 @@
     <div
       class="block"
       v-for="(category, indexCat) in formEcoSystem.data.eukaryoteComplexOptions"
-      :key="indexCat"
+      :key="indexCat + 10"
     >
       <b-field :label="category.name" custom-class="is-small"> </b-field>
       <div
@@ -92,7 +98,7 @@
           <label class="checkboxes-group__title">{{ group.name }}</label>
         </div>
         <div class="field" v-for="option in group.options" :key="option.id">
-          <b-checkbox v-model="riverEcosystem" :native-value="option">{{
+          <b-checkbox v-model="values.riverEcosystem" :native-value="option">{{
             option.name
           }}</b-checkbox>
         </div>
@@ -106,7 +112,7 @@
     >
       <b-field :label="category.name" custom-class="is-small"> </b-field>
       <div class="field" v-for="option in category.options" :key="option.id">
-        <b-checkbox v-model="riverEcosystem" :native-value="option">{{
+        <b-checkbox v-model="values.riverEcosystem" :native-value="option">{{
           option.name
         }}</b-checkbox>
       </div>
@@ -115,21 +121,23 @@
 </template>
 <script>
 import { getUserGeolocation } from "@/api/geolocation.js";
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   data() {
     return {
-      samplePointCoords: {
-        lon: 0,
-        lat: 0
-      },
-      samplePointWidth: 0,
-      samplePointDepth: 0,
-      samplePointWaterVelocity: 0,
-      samplePointWaterTemp: 0,
-      samplePointWaterTransparency: [],
-      riverEcosystem: []
+      values: {
+        samplePointCoords: {
+          lon: 0,
+          lat: 0
+        },
+        samplePointWidth: 0,
+        samplePointDepth: 0,
+        samplePointWaterVelocity: 0,
+        samplePointWaterTemp: 0,
+        samplePointWaterTransparency: [],
+        riverEcosystem: []
+      }
     };
   },
   computed: {
@@ -138,22 +146,28 @@ export default {
     }),
     flow() {
       return (
-        this.samplePointWidth *
-        this.samplePointDepth *
-        this.samplePointWaterVelocity
+        this.values.samplePointWidth *
+        this.values.samplePointDepth *
+        this.values.samplePointWaterVelocity
       );
     }
   },
+  beforeUpdate() {
+    this.updateSectionValues(this.values);
+  },
   methods: {
+    ...mapActions({
+      updateSectionValues: "updateSectionValues"
+    }),
     getActualPosition() {
       getUserGeolocation()
         .then(res => {
-          this.samplePointCoords.lon = parseFloat(res.coords.longitude).toFixed(
-            5
-          );
-          this.samplePointCoords.lat = parseFloat(res.coords.latitude).toFixed(
-            5
-          );
+          this.values.samplePointCoords.lon = parseFloat(
+            res.coords.longitude
+          ).toFixed(5);
+          this.values.samplePointCoords.lat = parseFloat(
+            res.coords.latitude
+          ).toFixed(5);
         })
         .catch(err => {
           //TODO: notificar?
