04bba46d1ce8f63224e26227bedfe0a21284ffc6
Luis Pedraz <LPedraz1981@gmail.com>
2020-01-29 11:07:15 +0100

    Calculate Ecology Status

--- src/components/FormEcoResult.vue
+++ src/components/FormEcoResult.vue
@@ -2,7 +2,7 @@
   <div class="form-section">
     <h5 class="title is-5">8. Estado ecológico</h5>
     <div class="results">
-      <div class="block">
+      <div class="block" v-if="ecoStatus !== null">
         <b-message :title="ecoStatus.name" type="is-info" :closable="false">
           {{ ecoStatus.description }}
           <div class="results__rate">
@@ -18,18 +18,72 @@
           </div>
         </b-message>
       </div>
+      <div class="block" v-if="!isStateEcoReady">
+        <b-message
+          title="Formulario incompleto"
+          type="is-warning"
+          :closable="false"
+        >
+          Hace falta completar la CALIDAD DEL AGUA y la CALIDAD DEL BOSQUE DE
+          RIBERA para ver el resultado del Estado Ecológico.
+        </b-message>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 export default {
+  name: "FormEcoResult",
   computed: {
     ...mapState({
-      formEcoResultData: state => state.formSections.ecoResult.data
+      statusOptions: state =>
+        state.formSections.ecoResult.data.ecologicalStateOptions,
+      riverQuality: state => state.formSections.riverQuality,
+      bioQuality: state => state.formSections.biological
+    }),
+    ...mapGetters({
+      isStateEcoReady: "isStateEcoReady"
     }),
     ecoStatus() {
-      return this.formEcoResultData.ecologicalStateOptions[0];
+      if (!this.isStateEcoReady) return null;
+      return this.calculateStatus();
+    }
+  },
+  methods: {
+    calculateStatus() {
+      const qrisiIndexValue = this.riverQuality.results.qrisiIndex.cat.value;
+      const bioQualityIndexValue = this.bioQuality.results.bioQualityIndex
+        .value;
+
+      if (qrisiIndexValue === 3) {
+        return this._getStatusForGoodQrisi(bioQualityIndexValue);
+      } else if (qrisiIndexValue === 2) {
+        return this._getStatusForIntermediateQrisi(bioQualityIndexValue);
+      } else if (qrisiIndexValue === 1) {
+        return this._getStatusForBadQrisi(bioQualityIndexValue);
+      }
+    },
+    _getStatusForGoodQrisi(bioQualityIndexValue) {
+      if (bioQualityIndexValue === 5) return this.statusOptions[0];
+      if (bioQualityIndexValue === 4) return this.statusOptions[1];
+      if (bioQualityIndexValue === 3) return this.statusOptions[2];
+      if (bioQualityIndexValue === 2) return this.statusOptions[3];
+      return this.statusOptions[4];
+    },
+    _getStatusForIntermediateQrisi(bioQualityIndexValue) {
+      if (bioQualityIndexValue === 5) return this.statusOptions[1];
+      if (bioQualityIndexValue === 4) return this.statusOptions[2];
+      if (bioQualityIndexValue === 3) return this.statusOptions[3];
+      if (bioQualityIndexValue === 2) return this.statusOptions[4];
+      return this.statusOptions[4];
+    },
+    _getStatusForBadQrisi(bioQualityIndexValue) {
+      if (bioQualityIndexValue === 5) return this.statusOptions[2];
+      if (bioQualityIndexValue === 4) return this.statusOptions[3];
+      if (bioQualityIndexValue === 3) return this.statusOptions[4];
+      if (bioQualityIndexValue === 2) return this.statusOptions[4];
+      return this.statusOptions[4];
     }
   }
 };
