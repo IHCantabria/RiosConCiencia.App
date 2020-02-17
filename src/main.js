6b93d9c494121d65762e71b66a5ff29771253972
David Coterillo Gonzalez <coterillogd@unican.es>
2020-02-17 16:01:04 +0100

    global mixin with control online

--- src/main.js
+++ src/main.js
@@ -12,8 +12,11 @@ import "@mdi/font/css/materialdesignicons.css";
 /** Global mixins */
 import { datesHelperMixin } from "@/mixins/dates-helper.js";
 import { androidBackButtonMixin } from "@/mixins/android-back-button.js";
+import { onlineHelperMixin } from "@/mixins/online-helper.js";
+
 Vue.mixin(datesHelperMixin);
 Vue.mixin(androidBackButtonMixin);
+Vue.mixin(onlineHelperMixin);
 
 Vue.use(VueOffline, {
   mixin: true,
