723b7732c42bd57ce2646cf4a2eebf987a09f0e0
David Coterillo Gonzalez <coterillogd@unican.es>
2020-03-02 16:35:16 +0100

    img radioButton

--- src/components/FormPicts/FormFlow.vue
+++ src/components/FormPicts/FormFlow.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="form-section">
-    <div class="header-section">
-      <a :href="pdfLink" class="header-section__help" target="_blank"
-        ><b-icon icon="information-outline" type="is-primary"></b-icon
-      ></a>
-    </div>
-    <b-field>
-      <b-radio-button v-model="values.waterFlow" :native-value="false">
-        <b-icon icon="close"></b-icon>
-        <span>No</span>
+    <b-field class="imgSection">
+      <b-radio-button
+        class="imgOption"
+        v-model="values.waterFlow"
+        :native-value="false"
+      >
+        <img :src="getImgUrl(1)" />
+        <div
+          :class="[
+            'overlay',
+            values.waterFlow == false ? 'overlay__active' : 'overlay__inactive'
+          ]"
+        ></div>
       </b-radio-button>
-      <b-radio-button v-model="values.waterFlow" :native-value="true">
-        <b-icon icon="check"></b-icon>
-        <span>Si</span>
+      <b-radio-button
+        class="imgOption"
+        v-model="values.waterFlow"
+        :native-value="true"
+      >
+        <img :src="getImgUrl(2)" />
+        <div
+          :class="[
+            'overlay',
+            values.waterFlow == true ? 'overlay__active' : 'overlay__inactive'
+          ]"
+        ></div>
       </b-radio-button>
     </b-field>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
-
+import { getBackground } from "@/utils/utils.js";
 export default {
   data() {
     return {
@@ -54,6 +67,11 @@ export default {
     ...mapActions({
       updateSpecificPictsSectionValues: "updateSpecificPictsSectionValues"
     }),
+    getImgUrl(iOption) {
+      const imgfolder = require.context("@/assets/images/picts/flow");
+      const optionImage = imgfolder(`./${getBackground(0, iOption)}`);
+      return optionImage;
+    },
     init() {
       this.values.waterFlow = null; //default value and make beforeUpdate hook jump
     }
