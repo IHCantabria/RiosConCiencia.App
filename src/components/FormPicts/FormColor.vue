2aca7c5098ec4bc348f8384291ebd23ff3fe5b4d
David Coterillo Gonzalez <coterillogd@unican.es>
2020-03-03 12:12:01 +0100

    section5

--- src/components/FormPicts/FormColor.vue
+++ src/components/FormPicts/FormColor.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="form-section">
-    <div class="header-section">
-      <a :href="pdfLink" class="header-section__help" target="_blank"
-        ><b-icon icon="information-outline" type="is-primary"></b-icon
-      ></a>
-    </div>
-    <b-field>
+    <b-field class="imgSection">
       <b-radio-button
+        :class="[
+          'imgOption',
+          isSelected(option) ? 'imgOption__active' : 'imgOption__inactive'
+        ]"
         v-for="option in formColor.data.colorRiverOptions"
         :key="option.id"
         :native-value="option"
         v-model="values.waterColor"
-        >{{ option.name }}</b-radio-button
-      >
+        ><img :src="getImgUrl(formColor.id, option.id)"/>
+        <div
+          :class="[
+            'overlay',
+            isSelected(option) ? 'overlay__active' : 'overlay__inactive'
+          ]"
+        ></div
+      ></b-radio-button>
     </b-field>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
-
+import { getBackground } from "@/utils/utils.js";
 export default {
   data() {
     return {
-      pdfLink: require("../../assets/pdfs/basico.pdf"),
+      imgFolder: null,
       values: {
         waterColor: 0
       }
@@ -32,11 +37,8 @@ export default {
     ...mapState({
       formColor: state => state.formPictsSections.color
     }),
-    waterColorHasErrors() {
-      return this.values.waterColor === null;
-    },
     isSectionValid() {
-      return !this.waterColorHasErrors;
+      return true; //optional section
     }
   },
   mounted() {
@@ -53,12 +55,24 @@ export default {
     ...mapActions({
       updateSpecificPictsSectionValues: "updateSpecificPictsSectionValues"
     }),
+    getImgUrl(idSection, iOption) {
+      return this.imgFolder
+        ? this.imgFolder(`./${getBackground(idSection, iOption)}`)
+        : "";
+    },
     init() {
+      this.imgFolder = require.context("@/assets/images/picts/color");
       this.values.waterColor = null; //default value and make beforeUpdate hook jump
+    },
+    isSelected(object) {
+      return this.values.waterColor == object;
     }
   }
 };
 </script>
 <style lang="scss" scoped>
 @import "@/styles/form-controls.scss";
+.imgOption {
+  width: 40%;
+}
 </style>
