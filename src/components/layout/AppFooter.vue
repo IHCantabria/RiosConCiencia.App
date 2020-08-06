c24a65562f9eeffe4102a760920bedf16722c6ea
David Coterillo Gonzalez <coterillogd@unican.es>
2020-08-06 11:04:01 +0200

    minor change maquetation logos

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="footer-container">
     <div class="footer-container__logos" v-show="!isFormPage">
-      <div class="claim-container">
-        <span>Con la colaboración de:</span>
+      <div class="logo">
+        <div class="claim-container">
+          <span>Con la colaboración de:</span>
+        </div>
+        <img
+          src="@/assets/logos/LogoMinisterio-FECYT.png"
+          title="Gobierno de España y Fundación Española para la Ciencia y la Tecnología"
+        />
       </div>
-      <img
-        src="@/assets/logos/LogoMinisterio-FECYT.png"
-        title="Gobierno de España y Fundación Española para la Ciencia y la Tecnología"
-      />
-      <img src="@/assets/logos/RedCambera.png" title="Red Cambera" />
-      <img src="@/assets/logos/ProyectoRios.png" title="Proyecto Ríos" />
+      <div class="logo">
+        <div class="claim-container">
+          <span>Un proyecto de:</span>
+        </div>
+        <img src="@/assets/logos/RedCambera.png" title="Red Cambera" />
+      </div>
+      <div class="logo-without-claim">
+        <img src="@/assets/logos/ProyectoRios.png" title="Proyecto Ríos" />
+      </div>
     </div>
     <div class="footer-container__form-nav" v-show="isFormPage">
       <form-fields-nav></form-fields-nav>
@@ -38,21 +47,38 @@ export default {
   flex-grow: 1;
   background-color: #fafafa;
   &__logos {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
     height: 100%;
-    & img {
-      max-height: 10vh;
-      padding: 0 1rem 1rem 1rem;
-    }
   }
   &__form-nav {
     margin: 1rem;
   }
 }
+.logo {
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  max-height: 10vh;
+  & img {
+    max-height: 6vh;
+    height: 100%;
+  }
+}
+.logo-without-claim {
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  max-height: 10vh;
+  & img {
+    max-height: 8vh;
+    height: 100%;
+  }
+}
 .claim-container {
+  display: flex;
   text-align: left;
   font-style: italic;
   font-weight: bold;
-  padding: 0.4rem 1rem 0 1rem;
+  margin-left: auto;
+  margin-right: auto;
   font-size: 0.7rem;
 }
 </style>
